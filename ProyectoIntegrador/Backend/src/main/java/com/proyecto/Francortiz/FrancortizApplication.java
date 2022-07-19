df9e6b739330396467a3c20fe1ed571216f8552d
Franco Ortiz <franco.ortiz25@gmail.com>
2022-07-19 12:33:06 -0300

    oo

---
+++ ProyectoIntegrador/Backend/src/main/java/com/proyecto/Francortiz/FrancortizApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.Francortiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrancortizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrancortizApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador/Backend/src/test/java/com/proyecto/Francortiz/FrancortizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.Francortiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrancortizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
